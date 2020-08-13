d6f3cf2ad711e1ce43bcd472345084420fbf56c4
Kevin Miller <kmillergit@outlook.com>
2020-08-13 07:30:49 -0700

    sidenav, prevSearches array added

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,20 @@ let prevSearches = [];
 console.log("page loaded")
 // api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={your api key}
 // api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={your api key}
-let main = () => {
-    cityNameForm.value = localStorage.getItem("cityName");
+function main() {
+    getLocalStorage();
+    addListeners();
 }
 
 $(document).ready(main);
+
+
+let getLocalStorage = () => {
+    cityNameForm.value = localStorage.getItem("cityName");
+    prevSearches = JSON.parse(localStorage.getItem("prevSearches"));
+    prevSearches === null ? prevSearches = [] : prevSearches = prevSearches;
+}
+
 
 let makeCall = (queryURL) => {
     return $.ajax({
@@ -20,19 +29,28 @@ let makeCall = (queryURL) => {
 }
 
 
-document.getElementById("location-form").addEventListener("submit", function() {
-    // The form, not the button, reloads the page. So for the submit event, the event must be on the form.
-    event.preventDefault();
-    let cityName = $("#city-name").val();
-    if (cityName === "") {
-        alert("You must enter a city!");
-    }
-    localStorage.setItem("cityName", cityName);
-    let queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKEY;
-    makeCall(queryURL).then(function (response) {
-        console.log(response);
-    }).catch(function(error) {
-        console.log("ERROR ERROR ERROR " + error.statusText);
-        console.log(error);
+let addListeners = () => {
+    document.getElementById("location-form").addEventListener("submit", function() {
+        // The form, not the button, reloads the page. So for the submit event, the event must be on the form.
+        event.preventDefault();
+
+        // Store the input city.
+        let cityName = $("#city-name").val();
+        if (cityName === "") {
+            alert("You must enter a city!");
+            return;
+        }
+        localStorage.setItem("cityName", cityName);
+
+        // Search forecast using input city.
+        let queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIKEY;
+        makeCall(queryURL).then(function (response) {
+            prevSearches.push(response);
+            localStorage.setItem("prevSearches", JSON.stringify(prevSearches));
+            console.log(response);
+        }).catch(function(error) {
+            console.log("ERROR ERROR ERROR " + error.statusText);
+            console.log(error);
+        });
     });
-})
+}
